4f5f2b86bae97c6f85b6eff8d12996ab92a61762
Piyush1816 <piyushgpt18@gmail.com>
2025-04-30 19:15:20 +0530

    Voice Detection Website

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def predict_route():
     return jsonify({'prediction': int(predicted_class)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render will set this environment variable
+    app.run(host="0.0.0.0", port=port)
